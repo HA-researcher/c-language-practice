3b403d47a23391affd751791506331105fc17ee2
HA | Student Researcher <haruka.ono.6q@stu.hosei.ac.jp>
2025-07-18 23:46:25 +0900

    ポインタで行列の内積を計算するコードを追加

---
+++ pointer_function.c
@@ -0,0 +1,33 @@
+/*「5次元のベクトル（5個の要素を持つfloat型配列）u[], v[]の内積を求め印刷するプログラム」
+ただし内積を求める計算はポインタを使用して行うものとする*/
+
+#include<stdio.h>
+
+float answerf(float a[5], float b[5]);
+
+int main(void){
+	float u[5];
+	float v[5];
+	for(int i=0; i<5; i++){
+		scanf("%f",&u[i]);
+	}
+	for(int i=0; i<5; i++){
+		scanf("%f",&v[i]);
+	}
+
+	printf("%f",answerf(u,v));
+
+}
+float answerf(float a[5], float b[5]){
+	float *u[5];
+	float *v[5];
+	float answer=0;
+	for(int i=0; i<5; i++){
+		u[i]=&a[i];
+		v[i]=&b[i];
+		answer+=*u[i]**v[i];
+	}
+	return answer;
+}
+
+
